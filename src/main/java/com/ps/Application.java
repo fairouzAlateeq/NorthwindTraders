acd5618d00f6924ef577f2aee75c7d794523870f
Fai Alateeq <fairouzalatteq@gmail.com>
2024-11-25 15:08:31 -0600

    added more info

--- src/main/java/com/ps/Application.java
+++ src/main/java/com/ps/Application.java
@@ -14,24 +14,34 @@ public class Application {
 
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Product ID: ");
+        boolean continueInput = true;
+
+        while (continueInput) {
+        System.out.println("Product ID(or type 'exit' to stop): ");
         String userInput = scanner.nextLine();
 
+        if (userInput.equalsIgnoreCase("exit")) {
+            continueInput = false;
+            break;
+        }
         String query = "SELECT * FROM products WHERE ProductID = ?" ;
         PreparedStatement statement = connection.prepareStatement(query);
         statement.setString(1, userInput);
         ResultSet results = statement.executeQuery();
 
         while (results.next()){
+            Integer productID = results.getInt("ProductID");
             String name = results.getString("ProductName");
             Double price = results.getDouble("unitPrice");
-            System.out.println(name + " " + price);
+            Integer unitsInStock = results.getInt("UnitsInStock");
+            System.out.println("productID:" + productID + ", name:" + name + ", price:" + price + ", Stock:" + unitsInStock);
+            System.out.println("---------------------------------------------------------");
         }
 
         results.close();
         statement.close();
+
+}
         connection.close();
-
-
     }
 }
